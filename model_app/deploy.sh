ca74f7e0e3ae6263709830bae5cd466bc60a8ae2
Khaela Fortunela <72397657+fortunelagit@users.noreply.github.com>
2023-05-29 20:01:00 +0700

    Update deploy.sh
    Update variables

--- model_app/deploy.sh
+++ model_app/deploy.sh
@@ -1,5 +1,5 @@
 GOOGLE_CLOUD_PROJECT="capstone-project-app-387808"
-
+REGION="us-central1"
 API_NAME="model-api-run"
 
 gcloud builds submit \
@@ -8,7 +8,7 @@ gcloud builds submit \
 gcloud run deploy $API_NAME \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/$API_NAME \
   --platform managed \
-  --region us-central1 \
+  --region $REGION \
   --cpu=8 \
   --memory=16Gi \
   --allow-unauthenticated \
