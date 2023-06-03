290dbac20b1f6e05055fa8e4101ffc4d27317cca
jejevj <wijaya.anggaa.j@gmail.com>
2023-06-03 07:27:42 +0700

    Add Camera Permission Dialog

--- app/src/main/java/com/capstoneproject/kukuku/MainActivity.kt
+++ app/src/main/java/com/capstoneproject/kukuku/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.capstoneproject.kukuku
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.capstoneproject.kukuku.ui.theme.KukukuApplicationTheme
 import com.capstoneproject.kukuku.ui.navigation.SetupNavGraph
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){
+        isGranted ->
+        if (isGranted){
+            Log.i("kilo","Permission Granted")
+        }else{
+            Log.i("kilo","Permission Denied")
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.primary
                 )
                 {
-
+                    requestCameraPermission()
                     val navController = rememberNavController()
                     SetupNavGraph(navController = navController)
 //                    Splash(alpha = 1f)
@@ -30,6 +49,23 @@ class MainActivity : ComponentActivity() {
 ////                    MyUI()
                 }
             }
+        }
+    }
+
+    private fun requestCameraPermission(){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED->{
+                Log.i("kilo","Permission previously granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            )->Log.i("kilo","Show camera permission dialog")
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
     }
 }
